119a20a14d20cda1651a713c967a8250c5e77a0b
Salomon Smeke <ssmeke@luc.edu>
2016-04-29 19:26:10 -0500

    foundations in JS

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,23 @@
 "use strict";
+
+var load = function(){
+
+}
+
+var update = function(arg){
+  d3.select("#algContainer").selectAll("div").style("background-color", function(d, i) {
+    return data[i];
+  });
+}
+
+function runEventLoop(loop){
+  var f;
+  for (f in loop) {
+    loop[f].name(loop[f].arg);
+  }
+}
+
+var loadLoop = [{name:update,arg:"Linear"},{name:load,arg:""}];
+var data = ["#FFF","#AAA","#FFF","#666","#FFF"];
+
+$(document).ready(runEventLoop(loadLoop));

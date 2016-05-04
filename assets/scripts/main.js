97021506095305ef84e9089c12176beadf03fbd1
Salomon Smeke <ssmeke@luc.edu>
2016-05-04 03:13:42 -0500

    added functionality

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,43 +1,57 @@
 "use strict";
-var generator
-var dualGenerator
+
+var generator;
+var dualGenerator;
+var curr, data;
+var lo = "12fa94";
+var hi = "fe4365";
 
 var load = function(){
-  generator = new LWGenPallete()
-  generator.setSteps(5)
-  generator.setStrength(90)
-  generator.setBase("fe4365");
-  dualGenerator = new LWGenBridge()
-  //bindFunctionsToDom
+  generator = new LWGenPallete();
+  generator.setSteps(5);
+  generator.setStrength(90);
+  dualGenerator = new LWGenBridge();
+  dualGenerator.setStepsBridge(5);
+  $(".ch").submit(function(e){
+    e.preventDefault();
+    letsDoIt(true);
+  });
+  $(".cl").submit(function(e){
+    e.preventDefault();
+    letsDoIt(false);
+  });
 }
 
 var update = function(arg){
-  //values should update before this.
+  curr = arg;
+  generator.setBaseS(hi);
+  dualGenerator.setBasesBridge([hi,lo]);
   switch (arg) {
     case "linear":
       generator.linear();
       data = generator.getGenerated();
       break;
     case "linearr":
-      generator.linear();
+      generator.linearR();
       data = generator.getGenerated();
       break;
     case "distill":
-      generator.linear();
+      generator.distill();
       data = generator.getGenerated();
       break;
     case "distillr":
-      generator.linear();
+      generator.distillR();
       data = generator.getGenerated();
       break;
     case "shade":
-      generator.linear();
+      generator.shade();
       data = generator.getGenerated();
       break;
     case "shader":
-      generator.linear();
+      generator.shadeR();
       data = generator.getGenerated();
       break;
+    case "bridge":
   }
   d3.select("#algContainer").selectAll("div").style("background-color", function(d, i) {
     return "#"+data[i];
@@ -51,8 +65,12 @@ function runEventLoop(loop){
   }
 }
 
-var loadLoop = [{name:load,arg:""},{name:update,arg:"linear"}];
-var data = ["#","#cb4365","#984365","#654365","#324365"];
+var letsDoIt = function (arg){
+  hi = document.getElementById("colorHigh").value;
+  lo = document.getElementById("colorLow").value;
+  update(curr);
+}
 
+var loadLoop = [{name:load,arg:""},{name:update,arg:"linear"}];
 
 $(document).ready(runEventLoop(loadLoop));
